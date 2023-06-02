bfcb2a993f5759abc74e8d143da32c009f563099
PrabhasKumarSreerama <sreeramaprabhaskumar18@gmail.com>
2023-06-02 18:59:15 +0530

    Added SearchBar Component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "../../Components/Home/Header";
+import SearchBar from "../../Components/Home/Search-bar";
 
 const Home = () => {
-  return <Header></Header>;
+  return (
+    <div>
+      <Header />
+      <SearchBar />
+    </div>
+  );
 };
 
 export default Home;
