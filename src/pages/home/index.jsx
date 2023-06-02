e1e90556198860db43e341a9ae56c5c9559ba3b3
PrabhasKumarSreerama <sreeramaprabhaskumar18@gmail.com>
2023-06-02 19:33:12 +0530

    HomePage Outline Ready

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../../Components/Home/Header";
 import SearchBar from "../../Components/Home/Search-bar";
+import BlogList from "../../Components/Home/Blog-posts";
+import { blogList } from "../../config/data";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <SearchBar />
+      <BlogList blogs={blogList} />
     </div>
   );
 };
