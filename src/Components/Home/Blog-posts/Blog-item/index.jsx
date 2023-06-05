efabaca97882eb5efb72a9ea122a5d4505db107d
PrabhasKumarSreerama <sreeramaprabhaskumar18@gmail.com>
2023-06-06 01:15:34 +0530

    blogitem created

--- src/Components/Home/Blog-posts/Blog-item/index.jsx
+++ src/Components/Home/Blog-posts/Blog-item/index.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import Chip from "../../../Common/Chip";
+import { Link } from "react-router-dom";
+import "./Style.css";
 
-const BlogItem = ({ blog }) => {
+const BlogItem = ({
+  blog: {
+    id,
+    title,
+    category,
+    description,
+    authorName,
+    authorAvatar,
+    createdAt,
+    cover,
+  },
+}) => {
   return (
-    <div>
-      <h1>BlogItem</h1>
+    <div className="blogitem">
+      <img className="blogitem-img" src={cover} alt="img not present" />
+      <Chip label={category} />
+      <h3>{title}</h3>
+      <p className="blog-desc">{description}</p>
+      <footer>
+        <div className="blogitem-author">
+          <img src={authorAvatar} alt="avatar" />
+          <div>
+            <h6>{authorName}</h6>
+            <p>{createdAt}</p>
+          </div>
+        </div>
+        <Link className="blogitem-link" to={`/blog/:${id}`}>
+          →
+        </Link>
+      </footer>
     </div>
   );
 };
