a2f77ada649a1c0e22e3e2ad6a7fac18dc7148a8
PrabhasKumarSreerama <sreeramaprabhaskumar18@gmail.com>
2023-06-06 17:31:04 +0530

    added functionality to the searchbar

---
+++ src/Components/Common/EmptyList/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Emptylist = () => {
+  return (
+    <div className="emptylist">
+      <img src="/assets/images/13525-empty.gif" alt="no results found" />
+    </div>
+  );
+};
+
+export default Emptylist;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../Components/Home/Header";
 import SearchBar from "../../Components/Home/Search-bar";
 import BlogList from "../../Components/Home/Blog-posts";
 import { blogList } from "../../config/data";
+import Emptylist from "../../Components/Common/EmptyList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(blogList);
+  const [searchKey, setSearchKey] = useState("");
+
+  const handleSearchBar = (e) => {
+    e.preventDefault();
+    handleSearchResults();
+  };
+
+  const handleSearchResults = () => {
+    const allBlogs = blogList;
+    const filteredBlogs = allBlogs.filter((blog) =>
+      blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
+    );
+    setBlogs(filteredBlogs);
+  };
+
+  const handleClearSearch = () => {
+    setBlogs(blogList);
+    setSearchKey("");
+  };
+
   return (
     <div>
       <Header />
-      <SearchBar />
-      <BlogList blogs={blogList} />
+      <SearchBar
+        value={searchKey}
+        formSubmit={handleSearchBar}
+        clearSearch={handleClearSearch}
+        handleSearchKey={(e) => setSearchKey(e.target.value)}
+      />
+      {!blogs.length ? <Emptylist /> : <BlogList blogs={blogs} />}
     </div>
   );
 };
